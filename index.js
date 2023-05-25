fdd402aeda3073600274d44b310a78d2afc25afb
oudin-clement <clementoudin3@gmail.com>
2023-05-25 11:33:04 +0200

    ui.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import photoloader from "./lib/photoloader.js";
-
+import photoloader from "./photoloader.js";
+import {displayPicture} from "./ui.js";
 
 const getPicture =
 
 getPicture(105);
-
+displayPicture(getPicture(105))
