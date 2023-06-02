8231aa6fc620a5c1a43d188f4aa4f469569b1906
Nico <nicolate.bernardet@gmail.com>
2023-06-02 14:47:17 +0200

    Fin étape 3

--- ui.js
+++ ui.js
@@ -3,3 +3,18 @@ import {imgSortie} from "./config.js";
 export function displayPicture(img){
     document.getElementById("la_photo").firstElementChild.setAttribute("src", `${imgSortie}/small/${img}`);
 }
+
+export const insertCategorie = categorieData => {
+    console.log(categorieData)
+    document.querySelector("#la_categorie")
+        .innerText = "categorie:"+categorieData.categorie.nom
+}
+
+export const insertComment = CommentData => {
+    console.log(CommentData)
+    CommentData.comments.forEach(com =>{
+        document.querySelector("#les_commentaires")
+            .innerText = com.pseudo+": "+com.content;
+    })
+
+}
